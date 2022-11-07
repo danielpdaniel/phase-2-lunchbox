1d8a00c9feddfac2fff47c5b07ba7f57a55e3d76
danielpdaniel <artofjquinn@gmail.com>
2022-11-07 18:37:55 -0500

    updates FoodItem cards and CSS to render the 'story' info for each food item when a little button on each respective card is clicked

--- src/components/FoodItem.js
+++ src/components/FoodItem.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function FoodItem({ food }){
+    const [clicked, setClicked] = useState(false)
+    
+    function handleClick(){
+        setClicked(clicked => !clicked)
+    }
     
     return (
-        <div className="foodItem">
+        <div className={!clicked ? "foodItem" : "foodItemExpanded"}>
             <h3> {food.name}</h3>
             <img src={food.image} alt={food.imageAlt}/>
+            {!clicked ? null :
+            <p>{food.story}</p>}
+            <button onClick={handleClick}>{!clicked ? "\u25BC" : "\u25B2"}</button>
         </div>
     )
 }
