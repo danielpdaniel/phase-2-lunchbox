dfedc6214f66d3a30064eebd279683e17931fc8b
danielpdaniel <artofjquinn@gmail.com>
2022-11-11 02:42:43 -0500

    changes header sizes on FoodItem tiles to be smaller

--- src/components/FoodItem.js
+++ src/components/FoodItem.js
@@ -17,13 +17,13 @@ function FoodItem({ food }){
     
     return (
         <div className={!clicked ? "foodItem" : "foodItemExpanded"}>
-            <h3> {food.name}</h3>
+            <h4> {food.name}</h4>
             <img src={food.image} alt={food.imageAlt}/>
         <br></br>
             <button onClick={handleClick} className={clicked? "clicked" : null}>{!clicked ? "Food Story \u25BC" : "Close \u25B2"}</button>
             {!clicked ? null :
             <div className="expandedFoodStory">
-            <h4>{food.origin}</h4>
+            <h5>{food.origin}</h5>
             <p>{food.story}</p>
             </div>}
         </div>
