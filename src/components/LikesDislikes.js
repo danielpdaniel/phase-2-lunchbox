03046330d6c5a39b3304382a548d07ad45465995
danielpdaniel <artofjquinn@gmail.com>
2022-11-14 22:52:27 -0500

    removes unused useEffect rom LikesDislikes component

--- src/components/LikesDislikes.js
+++ src/components/LikesDislikes.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 function LikesDislikes({ btnText, text, foodId, votesNum, onVote, clickedVotes }){
     const [clicked, setClicked] = useState(clickedVotes.includes(foodId+text) ? true : false);
